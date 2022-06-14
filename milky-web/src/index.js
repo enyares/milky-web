25be5e91706abbbc1e7bf6281209aba0605b4f14
elifisik <60474316+elifisik@users.noreply.github.com>
2022-06-14 23:05:21 +0300

    index.js

--- milky-web/src/index.js
+++ milky-web/src/index.js
@@ -16,7 +16,7 @@ import Headers from "./components/Header/Headers";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Headers />
+    <Header />
     <BrowserRouter>
       <App />
     </BrowserRouter>
